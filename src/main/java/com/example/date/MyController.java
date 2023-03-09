9182889e0b72a3020d40b00578e1bc70ea400ef3
Srinuuu <srinivasdasari042@gmail.com>
2023-03-09 09:18:30 +0000

    solution

---
+++ src/main/java/com/example/date/MyController.java
@@ -0,0 +1,18 @@
+package com.example.date;
+import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDate;
+
+@RestController 
+
+public class MyController {
+    @GetMapping("/")
+    public String date(){   
+        LocalDate localdate=LocalDate.now();
+        DateTimeFormatter dtf=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String str=localdate.format(dtf);
+        return str;
+    }
+}
